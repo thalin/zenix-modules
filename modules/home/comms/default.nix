886e78aea438ab064fd301e220abbe2030857d15
thalin <28450+thalin@users.noreply.github.com>
2024-10-14 13:51:47 -0400

    Allow unfree packages for nixpkgs

--- modules/home/comms/default.nix
+++ modules/home/comms/default.nix
@@ -6,13 +6,14 @@
   ...
 }:
 let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkEnableOption mkIf mkDefault;
   cfg = config.${namespace}.comms;
 in
 {
   options.${namespace}.comms.enable = mkEnableOption "zen home config: enable extra comms apps";
 
   config = mkIf cfg.enable {
+    nixpkgs.config.allowUnfree = mkDefault true;
     home.packages = with pkgs; [
       slack
       discord
