98f834136e130c5599b9a25d23c6a02f11ea6486
thalin <28450+thalin@users.noreply.github.com>
2024-10-14 20:29:17 -0400

    Add git module

---
+++ modules/home/git/default.nix
@@ -0,0 +1,46 @@
+{ 
+  lib,
+  pkgs,
+  namespace,
+  config,
+  ...
+}:
+let
+  inherit (lib) mkEnableOption mkIf mkOption types;
+  cfg = config.${namespace}.git;
+in
+{
+  options.${namespace}.git = {
+    enable = mkEnableOption "zen home config: enable git configuration";
+    userName = mkOption {
+      type = types.str;
+      default = "thalin";
+    };
+    userEmail = mkOption {
+      type = types.str;
+      default = "28450+thalin@users.noreply.github.com"; 
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      git
+    ];
+    programs.git = {
+      enable = true;
+      userName = cfg.userName;
+      userEmail = cfg.userEmail;
+      aliases = {
+        it = "!git init . && git add . && git commit --allow-empty -m 'First commit'";
+      };
+      extraConfig = {
+        push = {
+          autoSetupRemote = true;
+        };
+        init = {
+          defaultBranch = "main";
+        };
+      };
+    };
+  };
+}
