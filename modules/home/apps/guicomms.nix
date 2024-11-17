98aa84380a59c4490c7130f8189af87d64eb3d29
thalin <28450+thalin@users.noreply.github.com>
2024-11-16 21:15:57 -0800

    Move comms -> guicomms

--- modules/home/apps/guicomms.nix
+++ modules/home/apps/guicomms.nix
@@ -6,10 +6,10 @@
 }:
 let
   inherit (lib) mkEnableOption mkIf;
-  cfg = config.zen.apps.comms;
+  cfg = config.zen.apps.guicomms;
 in
 {
-  options.zen.apps.comms.enable = mkEnableOption "zen home config: enable extra comms apps";
+  options.zen.apps.guicomms.enable = mkEnableOption "zen home: guicomms apps";
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
