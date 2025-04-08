7d86ceeda895c92a2ece530bc9dbd11f165df82a
thalin <28450+thalin@users.noreply.github.com>
2025-04-08 11:21:12 -0400

    Add customization for ollama

--- modules/nixos/ai/ollama.nix
+++ modules/nixos/ai/ollama.nix
@@ -6,11 +6,16 @@
 }:
 let
   cfg = config.zen.ai.ollama;
-  inherit (lib) mkEnableOption mkPackageOption mkIf;
+  inherit (lib) mkOption mkEnableOption mkPackageOption mkIf types;
 in
 {
   options.zen.ai.ollama.enable = mkEnableOption "zen config: enable ollama";
   options.zen.ai.ollama.webui-pkg = mkPackageOption pkgs "open-webui" { };
+  options.zen.ai.ollama.host = mkOption {
+    default = "127.0.0.1";
+    type = types.str;
+    description = "zen config: override ollama host";
+  };
   
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [ ollama-rocm ];
@@ -19,7 +24,7 @@ in
         enable = true;
         acceleration = "rocm";
         rocmOverrideGfx = "11.0.1";
-        host = "127.0.0.1";
+        host = cfg.host;
         port = 11434;
         openFirewall = true;
       };
