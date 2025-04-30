9fb8aad03e11eec1d20d0eec820f6fdf4a1fea49
thalin <28450+thalin@users.noreply.github.com>
2025-04-30 13:29:41 -0700

    Add a bunch of git settings.

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -30,16 +30,90 @@ in
       userName = cfg.userName;
       userEmail = cfg.userEmail;
       aliases = {
-        it = "!git init . && git add . && git commit --allow-empty -m 'First commit'";
+        it = "!git init . && git commit --allow-empty -m 'First commit'";
       };
       extraConfig = {
-        push = {
-          autoSetupRemote = true;
+        core = {
+          compression = 9;
+          whitespace = "error";
+          preloadindex = true;
+        };
+        branch = {
+          sort = "-committerdate";
+        };
+        color = {
+          "decorate" = {
+            HEAD = "red";
+            branch = "blue";
+            tag = "yellow";
+            remoteBranch = "magenta";
+          };
+          "branch" = {
+            current = "magenta";
+            local = "default";
+            remote = "yellow";
+            upstream = "green";
+            plain = "blue";
+          };
+        };
+        diff = {
+          context = 3;
+          renames = "copies";
+          interHunkContext = 10;
         };
         init = {
           defaultBranch = "main";
         };
+        interactive = {
+          singlekey = true;
+        };
+        log = {
+          abbrevCommit = true;
+          graphColors = "blue,yellow,cyan,magenta,green,red";
+        };
+        pull = {
+          default = "current";
+          rebase = true;
+        };
+        push = {
+          autoSetupRemote = true;
+          default = "current";
+          followTags = true;
+        };
+        rebase = {
+          autoStash = true;
+          missingCommitsCheck = "warn";
+        };
+        status = {
+          branch = true;
+          showStash = true;
+          showUntrackedFiles = "all";
+        };
+        tag = {
+          sort = "-taggerdate";
+        };
+        url = {
+          "git@github.com:".insteadOf = "gh:";
+          "git@github.com:thalin/".insteadOf = "gt:";
+        };
       };
+      diff-so-fancy = {
+        enable = true;
+        markEmptyLines = false;
+      };
+    };
+    programs.zsh.shellAliases = {
+      gs  = "git status --short";
+      gd  = "git diff";
+      ga  = "git add";
+      gap = "git add --patch";
+      gc  = "git commit";
+      gp  = "git push";
+      gu  = "git pull";
+      gl  = "git log --all --graph --pretty=format:'%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n'";
+      gb  = "git branch";
+      gi  = "git it";
+      gcl = "git clone";
     };
   };
 }
