8b8163fcf7d6a9af447ab2a746b7457fd44ae6b5
thalin <28450+thalin@users.noreply.github.com>
2025-05-23 20:14:10 -0400

    Fix nerdfont for 25.05

--- modules/nixos/common/stylix.nix
+++ modules/nixos/common/stylix.nix
@@ -12,7 +12,7 @@
     };
     fonts = {
       monospace = {
-        package = (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; });
+        package = pkgs.nerd-fonts.fira-code;
         name = "FiraCode";
       };
       sansSerif = {
