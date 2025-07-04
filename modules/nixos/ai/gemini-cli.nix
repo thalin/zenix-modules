20c2a1794af8ba8e7d7689b1a5265f8ced3c9504
thalin <28450+thalin@users.noreply.github.com>
2025-07-04 11:37:08 -0400

    Add gemini-cli package

---
+++ modules/nixos/ai/gemini-cli.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.zen.ai.gemini-cli;
+  inherit (lib) mkEnableOption mkIf;
+in
+{
+  options.zen.ai.ollama.enable = mkEnableOption "zen config: enable gemini-cli";
+  
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ gemini-cli ];
+  };
+}
+
