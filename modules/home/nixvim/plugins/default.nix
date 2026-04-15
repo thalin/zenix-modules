963fcfb631151138e4e743fa5fa1e41edb382ce7
thalin <28450+thalin@users.noreply.github.com>
2026-04-15 16:20:52 -0400

    Make a light version of nixvim plugins.
    Full plugin list takes a lot of unnecessary space on low-disk machines
    like VMs.

--- modules/home/nixvim/plugins/default.nix
+++ modules/home/nixvim/plugins/default.nix
@@ -5,7 +5,7 @@
   ...
 }:
 let
-  inherit (lib) mkPackageOption mkEnableOption mkOption mkIf types;
+  inherit (lib) mkPackageOption mkEnableOption mkOption mkIf mkMerge types;
   cfg = config.zen.nixvim.plugins;
 in
 {
@@ -14,6 +14,7 @@ in
   ];
 
   options.zen.nixvim.plugins = {
+    light = mkEnableOption "zen config: light plugin loadout only";
     avante = {
       enable = mkEnableOption "zen config: enable avante plugin for nixvim";
       package = mkPackageOption pkgs.vimPlugins "avante-nvim" { };
@@ -34,101 +35,106 @@ in
 
   config = {
     programs.nixvim = {
-      plugins = {
-        airline = {
-          enable = true;
-          settings = {
-            powerline_fonts = 1;
+      plugins = mkMerge [
+        { # Light plugins, always included.
+          airline = {
+            enable = true;
+            settings = {
+              powerline_fonts = 1;
+            };
+          }; # airline
+          bufferline.enable = true;
+          oil.enable = true;
+          treesitter.enable = true;
+          ts-autotag.enable = true;
+          nix.enable = true;
+          vim-surround.enable = true;
+          comment.enable = true;
+          barbecue.enable = true;
+          which-key.enable = true;
+          web-devicons.enable = true;
+          luasnip.enable = true;
+          friendly-snippets.enable = true;
+          committia.enable = true;
+          coq-nvim.enable = true;
+          cmp = {
+            enable = true;
+            autoEnableSources = true;
+            settings = {
+              sources = [
+                {name = "nvim_lsp";}
+                {name = "path";}
+                {name = "buffer";}
+              ];
+              mapping = {
+                "<C-Space>" = "cmp.mapping.complete()";
+                "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+                "<C-e>" = "cmp.mapping.close()";
+                "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                "<CR>" = "cmp.mapping.confirm({ select = true })";
+                "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+                "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+              };
+            }; # settings
+          }; # cmp
+          cmp-nvim-lsp.enable = true;
+          cmp-buffer.enable = true;
+          cmp-path.enable = true;
+          cmp-cmdline.enable = true;
+          cmp-cmdline-history.enable = true;
+
+          telescope = {
+            enable = true;
+            extensions = {
+              fzf-native = {
+                enable = true;
+              };
+            };
+            settings.defaults.file_ignore_patterns = [ "^.git/" ];
+          }; # telescope
+          trouble = {
+            enable = true;
+            settings = {
+              auto_close = true;
+              auto_refresh = true;
+            };
           };
-        }; # airline
-        bufferline.enable = true;
-        oil.enable = true;
-        treesitter.enable = true;
-        ts-autotag.enable = true;
-        nix.enable = true;
-        vim-surround.enable = true;
-        comment.enable = true;
-        barbecue.enable = true;
-        which-key.enable = true;
-        web-devicons.enable = true;
-        luasnip.enable = true;
-        friendly-snippets.enable = true;
-        committia.enable = true;
-        coq-nvim.enable = true;
-        ollama.enable = true;
-        avante = mkIf cfg.avante.enable {
-          enable = true;
-          package = cfg.avante.package;
-          settings = {
-            # Note - currently ollama isn't supported by any released version
-            # As a result, we have to do this hacky thing:
-            # https://github.com/yetone/avante.nvim/wiki/Custom-providers#ollama
-            provider = "ollama";
-            providers = {
-              ollama = {
-                __inherited_from = "openai";
-                api_key_name = "";
-                endpoint = cfg.avante.ollama.endpoint;
-                model = cfg.avante.ollama.model;
-                disable_tools = true;
+        }
+
+        (lib.mkIf (!cfg.light) {
+          ollama.enable = true;
+          avante = mkIf cfg.avante.enable {
+            enable = true;
+            package = cfg.avante.package;
+            settings = {
+              # Note - currently ollama isn't supported by any released version
+              # As a result, we have to do this hacky thing:
+              # https://github.com/yetone/avante.nvim/wiki/Custom-providers#ollama
+              provider = "ollama";
+              providers = {
+                ollama = {
+                  __inherited_from = "openai";
+                  api_key_name = "";
+                  endpoint = cfg.avante.ollama.endpoint;
+                  model = cfg.avante.ollama.model;
+                  disable_tools = true;
+                };
               };
             };
           };
-        };
-        lsp = {
-          enable = true;
-          servers = {
-            nixd.enable = true;
-            pyright.enable = true;
-            html.enable = true;
-            marksman.enable = true;
-            dockerls.enable = true;
-            # nil_ls.enable = true;
-          }; # servers
-        }; # lsp
-        cmp = {
-          enable = true;
-          autoEnableSources = true;
-          settings = {
-            sources = [
-              {name = "nvim_lsp";}
-              {name = "path";}
-              {name = "buffer";}
-            ];
-            mapping = {
-              "<C-Space>" = "cmp.mapping.complete()";
-              "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-              "<C-e>" = "cmp.mapping.close()";
-              "<C-f>" = "cmp.mapping.scroll_docs(4)";
-              "<CR>" = "cmp.mapping.confirm({ select = true })";
-              "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-              "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-            };
-          }; # settings
-        }; # cmp
-        cmp-nvim-lsp.enable = true;
-        cmp-buffer.enable = true;
-        cmp-path.enable = true;
-        cmp-cmdline.enable = true;
-        cmp-cmdline-history.enable = true;
-
-        telescope = {
-          enable = true;
-          extensions = {
-            fzf-native = {
-              enable = true;
-            };
-          };
-          settings.defaults.file_ignore_patterns = [ "^.git/" ];
-        }; # telescope
-        trouble = {
-          enable = true;
-          settings = {
-            auto_close = true;
-            auto_refresh = true;
-          };
-        };
-      }; # plugins
+          lsp = {
+            enable = true;
+            servers = {
+              nixd.enable = true;
+              pyright.enable = true;
+              html.enable = true;
+              marksman.enable = true;
+              dockerls.enable = true;
+              # nil_ls.enable = true;
+            }; # servers
+          }; # lsp
+        })
+      ];# plugins
     };
   };
 }
