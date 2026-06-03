a72a48fe7d3974e215e007f2befaff640b9f4284
thalin <28450+thalin@users.noreply.github.com>
2026-06-03 12:52:43 -0400

    Moved google AI stuff to home-manager

---
+++ modules/home/ai/google-gemini.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.zen.ai.gemini-ai;
+  inherit (lib) mkEnableOption mkIf;
+in
+{
+  options.zen.ai.gemini-ai.enable = mkEnableOption "zen home: enable gemini-ai tools";
+  
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [ gemini-cli antigravity ];
+  };
+}
