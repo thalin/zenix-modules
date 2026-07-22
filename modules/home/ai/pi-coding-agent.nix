fcd786b706a41ed39a13ea63991d6c416beab41a
thalin <28450+thalin@users.noreply.github.com>
2026-07-22 14:14:27 -0400

    Add some options for pi config

--- modules/home/ai/pi-coding-agent.nix
+++ modules/home/ai/pi-coding-agent.nix
@@ -10,23 +10,40 @@ let
   inherit (lib) mkEnableOption mkIf;
 in
 {
-  imports = [
+  imports = mkIf cfg.enable [
     inputs.pi.homeModules.default
   ];
 
-  options.zen.ai.pi-coding-agent.enable = mkEnableOption "zen home: enable pi-coding-agent";
+  options.zen.ai.pi-coding-agent = {
+    enable = mkEnableOption "zen home: enable pi-coding-agent";
+    provider = lib.mkOption {
+      type = lib.types.str;
+      default = "google";
+      description = "The AI provider to use for pi-coding-agent";
+    };
+    model = lib.mkOption {
+      type = lib.types.str;
+      default = "gemini-3.5-flash";
+      description = "The AI model to use for pi-coding-agent";
+    };
+  };
 
   config = mkIf cfg.enable {
     sops.secrets.gemini_api_key = { };
+    sops.secrets.openrouter_api_key = { };
+
+    home.packages = [
+      pkgs.graphify
+    ];
 
     programs.pi.coding-agent = {
       enable = true;
       settings = {
-        provider = "google";
-        model = "gemini-3.5-flash";
+        inherit (cfg) provider model;
       };
       environment = {
         GEMINI_API_KEY = config.sops.secrets.gemini_api_key.path;
+        OPENROUTER_API_KEY = config.sops.secrets.openrouter_api_key.path;
       };
     };
   };
